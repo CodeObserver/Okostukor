7c903253f6a8e24da9f68f278f7c8029f1ed3eef
lendvaiaron <97970950+lendvaiaron@users.noreply.github.com>
2022-01-18 20:37:59 +0100

    Eljárások, függvények sqlite3-hez
    Adott kép beolvasása és átalakítása, valamint az adott kép eltárolásához hozunk létre egy sqlite3 file-t aminek segítségével eltudjuk tárolni a képeket

--- okostukor.py
+++ okostukor.py
@@ -16,3 +16,31 @@ from email.mime.image import MIMEImage
 from email.mime.text import MIMEText
 import time
 import sqlite3
+
+
+  
+#sqlitehoz ciklusok
+# sqlite adatbázis létrehozása függvény:
+def create_database():
+    # Image adatbázis létrehozása
+    image_database = sqlite3.connect("Image_data.db")
+    data = image_database.cursor()
+    a = data.execute("CREATE TABLE IF NOT EXISTS Image(Image BLOB)")
+    print(a)
+    image_database.commit()
+    image_database.close()
+# adott kép byte formára alakítása, majd annak a beolvasása a photo_image változóba és ezt a változót adja vissza a függvény
+def conver_image_into_binary(filename):
+    with open(filename, 'rb') as file:
+        photo_image = file.read()
+    return photo_image
+# ez egy eljárás, amely bekér egy képhez tartozó Path-t és ezt az adott képet elhelyezi az Image_data adatbázisban Blob-ként
+def insert_image(image):
+    image_database = sqlite3.connect("Image_data.db")
+    data = image_database.cursor()
+    insert_photo = conver_image_into_binary(image)
+    data.execute("INSERT INTO Image Values(:image)",
+                 {'image': insert_photo})
+    image_database.commit()
+    image_database.close()
+
